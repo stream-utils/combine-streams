9fff1db9985bebe3ab15a9702acf96334c59cbe2
Jonathan Ong <jonathanrichardong@gmail.com>
2013-09-13 00:03:01 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,104 @@
+var Stream = require('stream')
+var util = require('util')
+
+var PassThrough = Stream.PassThrough
+
+util.inherits(Combine, PassThrough)
+
+module.exports = Combine
+
+function Combine(options) {
+  if (!(this instanceof Combine))
+    return new Combine(options)
+
+  PassThrough.call(this, options)
+
+  this.queue = []
+  this.destroy = this.destroy.bind(this)
+}
+
+Combine.prototype.busy = false
+
+Combine.prototype.append = function (stream) {
+  if (!this._writableState.ended
+    && this.queue.push(stream) === 1
+    && !this.busy
+  ) this._next()
+
+  return this
+}
+
+Combine.prototype.destroy = function () {
+  if (!this.destroyed) {
+    this.emit('close')
+    this.destroyed = true
+    this.queue = null
+  }
+}
+
+Combine.prototype._append = function (stream) {
+  this.busy = true
+
+  stream.pipe(this, {
+    end: false
+  })
+
+  stream.once('error', callback)
+  stream.once('end', callback)
+  stream.once('close', this.destroy)
+  stream.once('close', cleanup)
+
+  var that = this
+
+  function callback(err) {
+    cleanup()
+
+    if (err)
+      that.emit('error', err)
+    else
+      that._next()
+  }
+
+  function cleanup() {
+    that.busy = false
+
+    stream.removeListener('error', callback)
+    stream.removeListener('end', callback)
+    stream.removeListener('close', that.destroy)
+    stream.removeListener('close', cleanup)
+  }
+}
+
+Combine.prototype._next = function (obj) {
+  if (this.destroyed)
+    return
+
+  if (arguments.length === 0)
+    obj = this.queue.shift()
+
+  if (obj === null) {
+    this.end()
+  } else if (obj === undefined) {
+    // Just ignore
+  } else if (typeof obj === 'string' || Buffer.isBuffer(obj)) {
+    this.write(obj)
+    this._next()
+  } else if (typeof obj === 'function') {
+    var that = this
+
+    this.busy = true
+
+    obj(function (err, obj2) {
+      if (err) {
+        that.emit('error', err)
+      } else {
+        that.busy = false
+        that._next(obj2)
+      }
+    })
+  } else if (obj instanceof Stream) {
+    this._append(obj)
+  } else {
+    throw new TypeError('You can only append streams, strings, buffers, and functions.')
+  }
+}
---
+++ test.js
@@ -0,0 +1,137 @@
+var assert = require('assert')
+var Stream = require('stream')
+var cat = require('cat-stream')
+
+var combine = require('./')
+
+var fixtures = {
+  string: 'string',
+  buffer: new Buffer('buffer', 'utf8'),
+  stream: function () {
+    var stream = new Stream.PassThrough()
+
+    stream.write('str')
+
+    setTimeout(function () {
+      stream.write('eam')
+      stream.end()
+    }, 3)
+
+    return stream
+  },
+  fn: function () {
+    return function (done) {
+      setTimeout(function () {
+        done(null, 'fn')
+      }, 1)
+    }
+  }
+}
+
+describe('Combine', function () {
+  it('should work when just pushing nothing', function (done) {
+    combine()
+    .append(null)
+    .pipe(cat(function (err, buf) {
+      assert.ifError(err)
+      assert.equal(buf.length, 0)
+      done()
+    }))
+  })
+  it('should work with just a stream', function (done) {
+    combine()
+    .append(fixtures.stream())
+    .append(null)
+    .pipe(cat(function (err, buf) {
+      assert.ifError(err)
+      assert.equal(buf.toString('utf8'), 'stream')
+      done()
+    }))
+  })
+
+  it('should work with just a string', function (done) {
+    combine()
+    .append(fixtures.string)
+    .append(null)
+    .pipe(cat(function (err, buf) {
+      assert.ifError(err)
+      assert.equal(buf.toString('utf8'), 'string')
+      done()
+    }))
+  })
+
+  it('should work with just a buffer', function (done) {
+    combine()
+    .append(fixtures.buffer)
+    .append(null)
+    .pipe(cat(function (err, buf) {
+      assert.ifError(err)
+      assert.equal(buf.toString('utf8'), 'buffer')
+      done()
+    }))
+  })
+
+  it('should work with just a function', function (done) {
+    combine()
+    .append(fixtures.fn())
+    .append(null)
+    .pipe(cat(function (err, buf) {
+      assert.ifError(err)
+      assert.equal(buf.toString('utf8'), 'fn')
+      done()
+    }))
+  })
+
+  it('should work with two streams', function (done) {
+    combine()
+    .append(fixtures.stream())
+    .append(fixtures.stream())
+    .append(null)
+    .pipe(cat(function (err, buf) {
+      assert.ifError(err)
+      assert.equal(buf.toString('utf8'), 'streamstream')
+      done()
+    }))
+  })
+
+  it('should work with a stream after a function', function (done) {
+    combine()
+    .append(fixtures.fn())
+    .append(fixtures.stream())
+    .append(null)
+    .pipe(cat(function (err, buf) {
+      assert.ifError(err)
+      assert.equal(buf.toString('utf8'), 'fnstream')
+      done()
+    }))
+  })
+
+  it('should ignore any objects after null', function (done) {
+    combine()
+    .append(fixtures.stream())
+    .append(null)
+    .append(fixtures.stream())
+    .pipe(cat(function (err, buf) {
+      assert.ifError(err)
+      assert.equal(buf.toString('utf8'), 'stream')
+      done()
+    }))
+  })
+
+  it('should work with delays between appends', function (done) {
+    var cmb = combine()
+    .append(fixtures.string)
+
+    cmb.pipe(cat(function (err, buf) {
+      assert.ifError(err)
+      assert.equal(buf.toString('utf8'), 'stringstring')
+      done()
+    }))
+
+    setTimeout(function () {
+      cmb
+      .append(fixtures.string)
+      .append(null)
+    }, 5)
+  })
+})
